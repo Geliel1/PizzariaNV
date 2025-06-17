7ed89e675eaf458de7c8cff24e23af81c61f22f7
Geliel Gustavo <85584428+Geliel1@users.noreply.github.com>
2025-06-17 13:53:22 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//import { initializeCart } from './cart.js';
+import { initializeModal } from './modal.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  //initializeCart();
+  initializeModal();  
+  
+});
